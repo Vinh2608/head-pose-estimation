61f2fa22e1767d5c6f24c84e01566e2f8df107b4
Vinh2608 <vinhluu1996@gmail.com>
2023-06-10 10:08:23 +0700

    modify for picture reading

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ https://github.com/yinguobing/head-pose-estimation
 from argparse import ArgumentParser
 
 import cv2
+import matplotlib.pyplot as plt
 
 from face_detection import FaceDetector
 from mark_detection import MarkDetector
@@ -34,13 +35,13 @@ def run():
     # Before estimation started, there are some startup works to do.
 
     # Initialize the video source from webcam or video file.
-    video_src = args.cam if args.video is None else args.video
-    cap = cv2.VideoCapture(video_src)
-    print(f"Video source: {video_src}")
+    img_src = args.cam if args.video is None else args.video
+    img = cv2.imread(img_src)
+    print(f"Video source: {img_src}")
 
     # Get the frame size. This will be used by the following detectors.
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_width = img.shape[0]
+    frame_height = img.shape[1]
 
     # Setup a face detector to detect human faces.
     face_detector = FaceDetector("assets/face_detector.onnx")
@@ -55,49 +56,38 @@ def run():
     tm = cv2.TickMeter()
 
     # Now, let the frames flow.
-    while True:
-
-        # Read a frame.
-        frame_got, frame = cap.read()
-        if frame_got is False:
-            break
-
-        # If the frame comes from webcam, flip it so it looks like a mirror.
-        if video_src == 0:
-            frame = cv2.flip(frame, 2)
 
         # Step 1: Get faces from current frame.
-        faces, _ = face_detector.detect(frame, 0.7)
+    faces, _ = face_detector.detect(img, 0.7)
 
         # Any valid face found?
-        if len(faces) > 0:
-            tm.start()
+    if len(faces) > 0:
+        tm.start()
 
             # Step 2: Detect landmarks. Crop and feed the face area into the
             # mark detector. Note only the first face will be used for
             # demonstration.
-            face = refine(faces, frame_width, frame_height, 0.15)[0]
-            x1, y1, x2, y2 = face[:4].astype(int)
-            patch = frame[y1:y2, x1:x2]
+        face = refine(faces, frame_width, frame_height, 0.15)[0]
+        x1, y1, x2, y2 = face[:4].astype(int)
+        patch = img[y1:y2, x1:x2]
 
             # Run the mark detection.
-            marks = mark_detector.detect([patch])[0].reshape([68, 2])
+        marks = mark_detector.detect([patch])[0].reshape([68, 2])
 
             # Convert the locations from local face area to the global image.
-            marks *= (x2 - x1)
-            marks[:, 0] += x1
-            marks[:, 1] += y1
+        marks *= (x2 - x1)
+        marks[:, 0] += x1
+        marks[:, 1] += y1
 
             # Step 3: Try pose estimation with 68 points.
-            pose = pose_estimator.solve(marks)
-
-            tm.stop()
+        pose = pose_estimator.solve(marks)
+        tm.stop()
 
             # All done. The best way to show the result would be drawing the
             # pose on the frame in realtime.
 
             # Do you want to see the pose annotation?
-            pose_estimator.visualize(frame, pose, color=(0, 255, 0))
+        pose_estimator.visualize(img, pose, color=(0, 255, 0))
 
             # Do you want to see the axes?
             # pose_estimator.draw_axes(frame, pose)
@@ -109,15 +99,15 @@ def run():
             # face_detector.visualize(frame, faces)
 
         # Draw the FPS on screen.
-        cv2.rectangle(frame, (0, 0), (90, 30), (0, 0, 0), cv2.FILLED)
-        cv2.putText(frame, f"FPS: {tm.getFPS():.0f}", (10, 20),
+        cv2.rectangle(img, (0, 0), (90, 30), (0, 0, 0), cv2.FILLED)
+        cv2.putText(img, f"FPS: {tm.getFPS():.0f}", (10, 20),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
 
         # Show preview.
-        cv2.imshow("Preview", frame)
+        cv2.imshow("Preview", img)
         if cv2.waitKey(1) == 27:
-            break
-
-
+            cv2.destroyAllWindows()
+        plt.imshow(img)
+        plt.show()
 if __name__ == '__main__':
     run()
